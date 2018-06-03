901620e41f2831d2f76dcb3f8e12570ffd0d9514
vagrant <vagrant@homestead>
2018-06-03 15:55:20 +0000

    finish followers

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   @if (Auth::check())
     <div class="row">
-       <div class="col-md-8">
+      <div class="col-md-8">
         <section class="status_form">
           @include('shared._status_form')
         </section>
@@ -14,7 +14,9 @@
         <section class="user_info">
           @include('shared._user_info', ['user' => Auth::user()])
         </section>
-
+        <section class="stats">
+          @include('shared._stats', ['user' => Auth::user()])
+        </section>
       </aside>
     </div>
   @else
